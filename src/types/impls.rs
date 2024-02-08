aaab4345d61c684fe786a5097b69b39f9f87407b
Nishant Joshi <nishant.joshi@juspay.in>
2024-02-08 22:13:45 +0530

    chore: do magic

--- src/types/impls.rs
+++ src/types/impls.rs
@@ -1,6 +1,15 @@
-
 impl<const N: usize> From<super::MinusOne<N>> for usize {
     fn from(_: super::MinusOne<N>) -> Self {
+        _ = <super::MinusOne<N> as AssertGt0>::VALIDATE;
+
         N - 1
     }
+}
+
+trait AssertGt0 {
+    const VALIDATE: ();
+}
+
+impl<const N: usize> AssertGt0 for super::MinusOne<N> {
+    const VALIDATE: () = assert!(N > 0, "N must be greater than 0");
 }
